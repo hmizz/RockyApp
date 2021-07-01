d2292c560b9d8ba566e2d5c2029f0666d1a077e9
hamez <hamez@MSI>
2021-07-01 09:35:21 +0100

    my commit

---
+++ Migrations/20210323142504_addCategoryToDatabase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocky.Migrations
+{
+    public partial class addCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ApplicationType",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Category",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "ApplicationType",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
